2d13e7faa5a6c092827eededa2a28ccb3b8e34b8
dymafr <contact@dyma.fr>
2023-11-03 10:32:57 +0100

    fix warning

--- src/router.jsx
+++ src/router.jsx
@@ -2,9 +2,6 @@ import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-const Admin = lazy(() => import('./pages/Admin/Admin'));
-
 export const router = createBrowserRouter([
   {
     path: '/',
@@ -12,11 +9,11 @@ export const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <HomePage />,
+        element: lazy(() => import('./pages/HomePage/HomePage')),
       },
       {
         path: 'admin',
-        element: <Admin />,
+        element: lazy(() => import('./pages/Admin/Admin')),
       },
     ],
   },
