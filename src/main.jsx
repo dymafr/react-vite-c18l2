017c9c6560cd4a7c8fac52afa54a57652a24ad45
Erwan Vallée <erwanp.vallee@gmail.com>
2025-06-05 15:57:29 +0200

    maj v7.6

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './assets/styles/index.scss';
 import { ApiContext } from './context/ApiContext';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
+import { RouterProvider } from 'react-router';
+import { ROUTER } from './router';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -11,7 +11,7 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <ApiContext.Provider value="https://restapi.fr/api/recipes">
-      <RouterProvider router={router} />
+      <RouterProvider router={ROUTER} />
     </ApiContext.Provider>
   </StrictMode>
 );
--- src/router.jsx
+++ src/router.jsx
@@ -1,24 +1,24 @@
 /* eslint-disable react-refresh/only-export-components */
 
 import { lazy } from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router';
 import App from './App';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const Admin = lazy(() => import('./pages/Admin/Admin'));
 
-export const router = createBrowserRouter([
+export const ROUTER = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    Component: App,
     children: [
       {
         index: true,
-        element: <HomePage />,
+        Component: HomePage,
       },
       {
         path: 'admin',
-        element: <Admin />,
+        Component: Admin,
       },
     ],
   },
